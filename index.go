aac821a6c1fee692155864b6da60baee723573ee
Adrian Mato <adrianmg@github.com>
2022-08-20 14:50:13 +0200

    fix: db is now working

--- index.go
+++ index.go
@@ -3,36 +3,50 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	// "net/http"
 	"os"
-	"time"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-type Stat struct {
-	id        int
-	slug      string
-	views     int
-	downloads int
-	createdAt time.Time
-	updatedAt time.Time
-}
-
 func main() {
 	loadEnv()
 	db := getDatabase()
+
+	ping, _ := db.DB()
+	if err := ping.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(ping.Stats().OpenConnections)
 
 	var stats []Stat
 	result := db.Find(&stats)
 	if result.Error != nil {
 		log.Fatal(result.Error)
 	}
-
 	fmt.Println(stats)
+}
+
+type Stat struct {
+	ID        int
+	Slug      string
+	Views     int
+	Downloads int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
 }
 
 func getDatabase() *gorm.DB {
@@ -46,12 +60,4 @@ func getDatabase() *gorm.DB {
 	}
 
 	return db
-}
-
-func loadEnv() {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatalf("Error loading .env file: %s", err)
-	}
 }
