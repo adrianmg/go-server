c229b16c11aaf23203b810751987db65e8166771
Adrian Mato <adrianmg@github.com>
2022-08-20 03:30:54 +0200

    testing spaces for indentation

--- index.go
+++ index.go
@@ -8,29 +8,29 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", getTime)
-	http.HandleFunc("/ip", getIp)
+  http.HandleFunc("/", getTime)
+  http.HandleFunc("/ip", getIp)
 
-	http.ListenAndServe(getServerPort(), nil)
+  http.ListenAndServe(getServerPort(), nil)
 }
 
 func getTime(res http.ResponseWriter, req *http.Request) {
-	now := time.Now()
+  now := time.Now()
 
-	fmt.Fprintf(res, "The time is %s\n", now.Format("3:04:05 PM"))
+  fmt.Fprintf(res, "The time is %s\n", now.Format("3:04:05 PM"))
 }
 
 func getIp(res http.ResponseWriter, req *http.Request) {
-	ip := req.RemoteAddr
+  ip := req.RemoteAddr
 
-	fmt.Fprintf(res, "Your IP is %s\n", ip)
+  fmt.Fprintf(res, "Your IP is %s\n", ip)
 }
 
 func getServerPort() string {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+  }
 
-	return ":" + port
+  return ":" + port
 }
